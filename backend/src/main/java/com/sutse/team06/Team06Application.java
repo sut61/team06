ee08cdf1e78e04f97e8cbba0455bf55d2e247635
dtmkeng <kang081@hotmail.com>
2018-12-18 18:12:28 +0700

    add backend

---
+++ backend/src/main/java/com/sutse/team06/Team06Application.java
@@ -0,0 +1,14 @@
+package com.sutse.team06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team06Application.class, args);
+	}
+
+}
+
