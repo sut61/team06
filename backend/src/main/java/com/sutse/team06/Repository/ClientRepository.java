66b73a5e088c3c32f68240283dafae88b204b5ee
supanutjarukulgowit <supanutjarukulgowit@hotmail.com>
2019-01-04 19:51:09 +0700

    close #42 - fix bug in repository

--- backend/src/main/java/com/sutse/team06/Repository/ClientRepository.java
+++ backend/src/main/java/com/sutse/team06/Repository/ClientRepository.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    Client findByCid(Long Cid);
+    Client findByCid(Long cid);
     Client findByName(String name);
-    Client findByUsernameAndPassword(String  username,String password);
+    Client findByUsernameAndPassword(String username,String password);
 }
