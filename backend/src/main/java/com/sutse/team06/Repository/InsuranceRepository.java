c82fddfbc32c196597d2aab6b936785c59c7a796
supanutjarukulgowit <supanutjarukulgowit@hotmail.com>
2019-01-04 17:36:17 +0700

    close #30 - add Repository

--- backend/src/main/java/com/sutse/team06/Repository/InsuranceRepository.java
+++ backend/src/main/java/com/sutse/team06/Repository/InsuranceRepository.java
@@ -5,7 +5,6 @@ import com.sutse.team06.entity.Insurance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface InsuranceRepository extends JpaRepository<Insurance, Long> {
