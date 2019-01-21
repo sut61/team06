c9db7cc31775d4053fcb59e3ffae18cbf82431b7
dtmkeng <kang081@hotmail.com>
2019-01-21 16:38:00 +0700

    fix error insurance

--- backend/src/main/java/com/sutse/team06/Repository/InsuranceTypeRepository.java
+++ backend/src/main/java/com/sutse/team06/Repository/InsuranceTypeRepository.java
@@ -2,7 +2,6 @@ package com.sutse.team06.repository;
 
 
 import com.sutse.team06.entity.InsuranceType;
-import com.sutse.team06.entity.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
