9812381028a8879b88183ad910cc5b52e51042cd
pheekaythailand <PheekayThailand@gmail.com>
2019-01-21 22:52:55 +0700

    close #63 -addFoodRepositoryandUpdate

--- backend/src/main/java/com/sutse/team06/Repository/FoodListRepository.java
+++ backend/src/main/java/com/sutse/team06/Repository/FoodListRepository.java
@@ -1,15 +1,14 @@
 package com.sutse.team06.Repository;
 
+import com.sutse.team06.entity.FoodList;
 
-import com.sutse.team06.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-     Employee findByEmpId(Long empId);
-     Employee findByUsernameAndPassword(String  username,String password);
-     Employee findByname(String name); //pheekay
+public interface FoodListRepository extends JpaRepository<FoodList, Long> {
+     FoodList findByfoodlistId(Long foodlistId);
+     FoodList findByfoodlistName(String foodlistName);
 }
