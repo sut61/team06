6ab7fe62b0592387e64b7333e6564e8dc3912967
dtmkeng <kang081@hotmail.com>
2019-01-22 21:52:12 +0700

    ทำ เเก้ไข้ annotation ทุก enitty ของ ระบบจัดการพัสดุ  -close #64

--- backend/src/main/java/com/sutse/team06/entity/DeliveryCompany.java
+++ backend/src/main/java/com/sutse/team06/entity/DeliveryCompany.java
@@ -6,18 +6,17 @@ import lombok.*;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.GeneratedValue;
+import javax.validation.constraints.NotNull;
 import javax.persistence.GenerationType;
 import javax.persistence.Entity;
 
 @Entity
-@Getter @Setter
-@NoArgsConstructor
-@ToString @EqualsAndHashCode
+@Data
 public class DeliveryCompany {
     @Id
     @SequenceGenerator(name = "DeliveryCompany_seq", sequenceName = "DeliveryCompany_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "DeliveryCompany_seq")
-    private @NonNull Long deliComId;
-    private @NonNull String name;
+    @NotNull private  Long deliComId;
+    @NotNull private  String name;
   
 }
