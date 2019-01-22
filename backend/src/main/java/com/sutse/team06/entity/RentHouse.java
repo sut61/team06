a26a728926a37690b2f6f785458481e87e170b8c
jumptheeraphat <jumpdevil@hotmail.com>
2019-01-22 23:39:17 +0700

    close #68 - add relation on RentHouse.java entity

--- backend/src/main/java/com/sutse/team06/entity/RentHouse.java
+++ backend/src/main/java/com/sutse/team06/entity/RentHouse.java
@@ -4,10 +4,13 @@ package com.sutse.team06.entity;
 import lombok.*;
 
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 
 @Entity
 @Getter @Setter
@@ -18,4 +21,13 @@ public class RentHouse {
     @SequenceGenerator(name = "RentHouse_seq", sequenceName = "RentHouse_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "RentHouse_seq")
     private @NonNull Long rentId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="renttypeId")
+    private RentHouseType rentHouseType;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="houseId")
+    private House house;
+
 }
