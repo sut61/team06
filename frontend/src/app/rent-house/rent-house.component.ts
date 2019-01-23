bfc903ceb5a4ba62e5c3c6a8cdfe808d703042cf
jumptheeraphat <jumpdevil@hotmail.com>
2019-01-23 22:38:21 +0700

    close #83 - เพิ่มUIของระบบเช้าบ้านเบื้องต้น

---
+++ frontend/src/app/rent-house/rent-house.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rent-house',
+  templateUrl: './rent-house.component.html',
+  styleUrls: ['./rent-house.component.css']
+})
+export class RentHouseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
