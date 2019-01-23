81b74f66b7d9bc306a920127b28b9104ed5de961
jumptheeraphat <jumpdevil@hotmail.com>
2019-01-24 02:54:08 +0700

    close #87 - เพิ่ม CrossOrigin ใน controller

--- backend/src/main/java/com/sutse/team06/Controller/RentHouseController.java
+++ backend/src/main/java/com/sutse/team06/Controller/RentHouseController.java
@@ -10,8 +10,10 @@ import com.sutse.team06.entity.*;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 class RentHouseController {
     @Autowired
     private HouseRepository houseRepository;
