e19bd82a516f5ab49d26db7e7433677d35004822
dtmkeng <kang081@hotmail.com>
2019-01-23 18:28:48 +0700

    ทำ เชื่อมเเต่ละหน้า เข้ากับหน้าหลัก -close #72

---
+++ frontend/src/app/manage-package-main/manage-package-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-package-main',
+  templateUrl: './manage-package-main.component.html',
+  styleUrls: ['./manage-package-main.component.css']
+})
+export class ManagePackageMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
