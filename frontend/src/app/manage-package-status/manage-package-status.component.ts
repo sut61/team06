36398d22851dadcbb97850fc335269bd4b773c58
dtmkeng <kang081@hotmail.com>
2019-01-24 21:37:10 +0700

    ทำ service ฝั่ง frontend หน้า เเสดง สถานะพัสดุ -close #75

--- frontend/src/app/manage-package-status/manage-package-status.component.ts
+++ frontend/src/app/manage-package-status/manage-package-status.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MangagePackageService } from '../service/mangage-package.service';
+import { Router } from '@angular/router';
+import { async } from '@angular/core/testing';
 @Component({
   selector: 'app-manage-package-status',
   templateUrl: './manage-package-status.component.html',
   styleUrls: ['./manage-package-status.component.css']
 })
 export class ManagePackageStatusComponent implements OnInit {
-
-  constructor() { }
-
+  statusList: any = [];
+  constructor(private mangagePackageService: MangagePackageService) { }
   ngOnInit() {
+      this.mangagePackageService.getManageAll().subscribe(data => {
+          console.log(data);
+          data.forEach( (element, id) => {
+              this.mangagePackageService.checkPackeIn(element.mpInId).subscribe(check => {
+                  if (check == null) {
+                       data[id].status = 'In Stock';
+                  } else {
+                      data[id].status = 'Out Stock';
+                  }
+              });
+          });
+          this.statusList = data;
+          // this.mangagePackageService.checkPackeIn(data.mpInId)
+      });
+      console.log(this.statusList);
   }
 
 }
