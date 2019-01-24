a63f7966a16a3c854fd7837648e3f6031998393a
yuug9 <yuu.meechai@gmail.com>
2019-01-24 19:49:07 +0700

    ทำ RepairsRepository ของระบบแจ้งซ่อม -close #97

---
+++ backend/src/main/java/com/sutse/team06/Repository/RepairsRepository.java
@@ -0,0 +1,14 @@
+package com.sutse.team06.repository;
+
+
+import com.sutse.team06.entity.Repairs;
+import com.sutse.team06.entity.RentHouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+public interface RepairsRepository extends JpaRepository<Repairs, Long> {
+     Repairs findByRepairsId(Long repairsId);
+}
