5ad4cf66e9294d8eafe9f26fc926717c5a82c179
supanutjarukulgowit <supanutjarukulgowit@hotmail.com>
2019-01-25 17:13:55 +0700

    -close #113 fix bug in Controller,Repository and add feild

--- backend/src/main/java/com/sutse/team06/Controller/InsuranceController.java
+++ backend/src/main/java/com/sutse/team06/Controller/InsuranceController.java
@@ -33,10 +33,21 @@ public class InsuranceController {
     public  Client getClientByCid(@PathVariable("cid") Long cid){
      return this.clientRepository.findByCid(cid);
     }
+    @GetMapping("/Client")
+    public List<Client> ClientAll(){
+        return clientRepository.findAll().stream().collect(Collectors.toList());
+    }
 
-    @PostMapping("/client/{name}")
-    public  Client client(@PathVariable("name") String name){
-         return this.clientRepository.findByName(name);
+
+    @PostMapping("/insurance/save/{name}/{type}/{price}/{insuname}")
+    public  Insurance insurance(@PathVariable Long name,@PathVariable Long type,
+    @PathVariable Long price,@PathVariable String insuname){
+        Insurance insurance = new Insurance();
+        insurance.setClient(clientRepository.getOne(name));        
+        insurance.setType(insuranceTypeRepository.getOne(type));
+        insurance.setPrice(insurancePriceRepository.getOne(price));
+        insurance.setInsuname(insuname);
+        return insuranceRepository.save(insurance);
     }
   /*  @GetMapping("/client")
     public List<Client> Client(){
@@ -48,12 +59,20 @@ public class InsuranceController {
     public InsuranceType getTypeByTid(@PathVariable("tid") Long tid){
      return this.insuranceTypeRepository.findByTid(tid);
     }
+    @GetMapping("/InsuranceType")
+    public List<InsuranceType> InsuranceTypeAll(){
+        return insuranceTypeRepository.findAll().stream().collect(Collectors.toList());
+    }
     
     // Price
-    @GetMapping("/insurancePrice/{pid}")
-    public InsurancePrice getTypeByPrice(@PathVariable("pid") Long pid){
-     return this.insurancePriceRepository.findByPid(pid);
+    @GetMapping("/InsurancePrice")
+    public List<InsurancePrice> InsurancePriceAll(){
+        return insurancePriceRepository.findAll().stream().collect(Collectors.toList());
     }
-
+    @GetMapping("/InsurancePriceSelect/{tid}")
+    public List<InsurancePrice> InsurancePriceSelectAll(@PathVariable("tid") Long tid){
+        InsuranceType type = this.insuranceTypeRepository.findByTid(tid);
+        return this.insurancePriceRepository.findByType(type);
+    }
    
 }
