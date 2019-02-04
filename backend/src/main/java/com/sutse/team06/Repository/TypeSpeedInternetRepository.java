708baf928b71d55ccb4a0338f1517de93eed5aea
dtmkeng <kang081@hotmail.com>
2019-02-05 00:29:31 +0700

    ทำ repository class เเละ create query function -close #135

--- backend/src/main/java/com/sutse/team06/Repository/TypeSpeedInternetRepository.java
+++ backend/src/main/java/com/sutse/team06/Repository/TypeSpeedInternetRepository.java
@@ -1,16 +1,13 @@
 package com.sutse.team06.Repository;
 
 
-import com.sutse.team06.entity.RentHouse;
-import com.sutse.team06.entity.House;
+import com.sutse.team06.entity.TypeSpeedInternet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import java.util.List;
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
-public interface RentHouseRepository extends JpaRepository<RentHouse, Long> {
-     RentHouse findByRentId(Long rentId);
-     RentHouse findByHouse(House house);
-     RentHouse findByRentIdAndHouse(Long rentId,House house);
+public interface TypeSpeedInternetRepository extends JpaRepository<TypeSpeedInternet, Long> {
+   TypeSpeedInternet findByTypeSpeedId(Long typeSpeedId);
 }
