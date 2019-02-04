41040c2e71cf46e54d7a9005f40687e850c5dd65
jumptheeraphat <jumpdevil@hotmail.com>
2019-02-04 16:00:13 +0700

    close #123 - เพิ่ม repo กับ controller ที่เกี่ยวกับ ระบบยกเลิกสัญญา

---
+++ backend/src/main/java/com/sutse/team06/Repository/CancelTypeRepository.java
@@ -0,0 +1,13 @@
+package com.sutse.team06.Repository;
+
+
+import com.sutse.team06.entity.CancelType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+public interface CancelTypeRepository extends JpaRepository<CancelType, Long> {
+    
+}
