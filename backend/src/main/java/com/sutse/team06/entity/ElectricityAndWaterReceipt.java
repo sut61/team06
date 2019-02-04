de0d6af997811158e0987ad79ec19fdb444484e3
supanutjarukulgowit <supanutjarukulgowit@hotmail.com>
2019-02-04 21:05:03 +0700

    close - fix file name and relation

---
+++ backend/src/main/java/com/sutse/team06/entity/ElectricityAndWaterReceipt.java
@@ -0,0 +1,39 @@
+package com.sutse.team06.entity;
+
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+@Data
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+public class ElectricityAndWaterReceipt {
+    @Id
+    @SequenceGenerator(name = "ElctricityAndWaterReceipt_seq", sequenceName = "ElctricityAndWaterReceipt_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ElctricityAndWaterReceipt_seq")
+    private @NonNull Long ewid;
+    private @NonNull Long water;
+    private @NonNull Long electircity;
+    private @NonNull Long wesum;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "Cid")
+    private Client cid;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="houseId")
+    private House house;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "Mid")
+    private Mounth mid;
+}
