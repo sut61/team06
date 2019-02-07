380979ee388ceaba5d1e4c8cb5223d0c552c67ef
dtmkeng <kang081@hotmail.com>
2019-02-07 22:15:27 +0700

    ทำ ส่วน service เเล้ว function submit Data -close #157

--- frontend/src/app/request-internet/request-internet.component.ts
+++ frontend/src/app/request-internet/request-internet.component.ts
@@ -1,22 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-request-internet',
   templateUrl: './request-internet.component.html',
   styleUrls: ['./request-internet.component.css']
 })
 export class RequestInternetComponent implements OnInit {
+  detail: any = {};
+  reqinternet: any = {};
+  timeType: Array<any>;
+  TypeSpeed: Array<any>;
   state1: boolean;
   state2: boolean;
   noti: boolean;
   noti2: boolean;
   notimag: String;
+  // page1 01
+  // page2 10
+  // page3 11
   constructor() { }
 
   ngOnInit() {
-    this.state1 = true;
-    this.state2 = false;
+     this.state1 = true;
+     this.state2 = false;
+     this.noti = true;
+     this.noti2 = true;
+
   }
+  onCheck() {
+  }
+  onSave() {
 
-
+  }
+  speedCheck() {
+    const  { netname, netpassword, typeSpeedId, timeTypeId, rentid } = this.reqinternet;
+  }
 }
+
