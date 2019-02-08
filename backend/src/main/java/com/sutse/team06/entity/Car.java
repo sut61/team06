b33df9e8cf1fa936a805c166d47f0be997ba7e5b
pheekaythailand <PheekayThailand@gmail.com>
2019-02-09 00:32:25 +0700

    close #164 -Car.java

---
+++ backend/src/main/java/com/sutse/team06/entity/Car.java
@@ -0,0 +1,27 @@
+package com.sutse.team06.entity;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+
+@Data
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+@Table(name="Car")
+public class Car{
+ @Id
+	@SequenceGenerator(name="Car_seq",sequenceName="Car_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="Car_seq")
+	@Column(name="carId",unique = true, nullable = true)
+	private @NonNull Long carId;
+	private @NonNull String carName;
+    
+	
+	
+}
